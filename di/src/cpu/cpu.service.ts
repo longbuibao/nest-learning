05f1b24a2daaf721a7c41035d4833b078c585d2f
longbuibao <blong1102@gmail.com>
2021-11-22 12:22:54 +0700

    computer DI

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PowerService } from 'src/power/power.service'
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number): number {
+    console.log('Drawing 10 watts from Power Service')
+    this.powerService.supplyPower(10)
+    return a + b
+  }
+}
--- di/src/disk/disk.service.ts
+++ di/src/disk/disk.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PowerService } from 'src/power/power.service'
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData() {
+    console.log('Powering 10 watts for disk ')
+    this.powerService.supplyPower(20)
+    return 'Data'
+  }
+}

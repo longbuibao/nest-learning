805127de24df7347b1209b0be15de6b4df4dac24
longbuibao <blong1102@gmail.com>
2021-11-23 09:03:18 +0700

    setup new project

--- di/src/power/power.module.ts
+++ di/src/power/power.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { PowerService } from './power.service';
+import { Module } from '@nestjs/common'
+import { PowerService } from './power.service'
 
 @Module({
+  //create DI container
   providers: [PowerService],
+  //inside DI container, indicate that this class can be used in other class
   exports: [PowerService],
 })
 export class PowerModule {}

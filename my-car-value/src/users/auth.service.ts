27c754e1fd974ad598785fe7e95b7ec9262108ba
longbuibao <blong1102@gmail.com>
2021-11-28 11:55:49 +0700

    if email in use

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { UsersService } from './users.service'
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // if email in use
+    const users = await this.usersService.find(email)
+    if (users.length) throw new BadRequestException('Email in use')
+    //hash password
+    //create user
+    //return new user
+  }
+
+  signin() {}
 }

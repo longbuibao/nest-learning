bc453145b9f1c1247f0eb75a2dec447588d90ea1
bui bao long <blong1102@gmail.com>
2022-01-19 18:24:45 +0700

    delete all

--- di/src/power/power.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PowerService {
-  supplyPower(watts: number) {
-    console.log(`Supplying ${watts} worth of power.`);
-  }
-}
-// see ya tomorrow

26fef1082599e527641a0b08dc854c36adb5dea4
bui bao long <blong1102@gmail.com>
2022-01-26 21:48:25 +0700

    NotFoundException

--- car-value/src/users/users.service.ts
+++ car-value/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -20,7 +20,7 @@ export class UsersService {
   async update(id: string, atts: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     Object.assign(user, atts);
     return this.repo.save(user);
@@ -28,7 +28,7 @@ export class UsersService {
   async remove(id: string) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     return this.repo.remove(user);
   }

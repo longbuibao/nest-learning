106ef82fcf34ec6df334d281b9c1a9a3b822d68e
bui bao long <blong1102@gmail.com>
2022-02-12 21:47:29 +0700

    testing

---
+++ car-value/test/auth.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a singup request', () => {
+    const email = 'blong1102@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'kfjdslafjsdklf',
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+
+  it('handles a singup request', () => {
+    const email = 'blong1102@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'kfjdslafjsdklf',
+      })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});

d16ce7ddbab91992cee5e1de0d659cddf8ab8326
bui bao long <blong1102@gmail.com>
2022-02-15 16:05:32 +0700

    report dtos

---
+++ car-value/src/reports/dtos/report.dto.ts
@@ -0,0 +1,34 @@
+import { Expose, Transform } from 'class-transformer';
+import { User } from '../../users/user.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  mileage: string;
+
+  @Transform(({ obj }) => {
+    return obj.user.id;
+  })
+  @Expose()
+  userId: number;
+}
